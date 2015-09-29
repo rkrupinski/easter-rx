6c100fe89c396da2b89dfa1f54874124e052258a
Rafał Krupiński <krupinski.rafal@gmail.com>
2015-09-29 14:55:46 +0200

    🐷

--- src/easter-rx.js
+++ src/easter-rx.js
@@ -4,32 +4,35 @@ const source = Rx.Observable.fromEvent(window, 'keyup');
 const gap = { gap: true };
 const delay = 500;
 
-const checkSequence = (data) =>
-    data.buf.toString() === data.sequence.toString();
 
 export default (sequence, callback) => {
+  const pattern = sequence.toString();
+
   let sub = source
-      .timestamp()
+      .map(evt => evt.keyCode)
+      .timeInterval()
       .startWith(Object.create(gap))
       .bufferWithCount(2, 1)
       .concatMap(buf => {
         let ret;
 
-        if (buf[0].gap) {
-          ret = Rx.Observable.fromArray(buf);
-        } else if (buf[1].timestamp - buf[0].timestamp <= delay) {
-          ret = Rx.Observable.just(buf[1]);
-        } else {
-          ret = Rx.Observable.of(Object.create(gap), buf[1]);
+        switch (true) {
+          case buf[0].gap:
+            ret = Rx.Observable.fromArray(buf);
+            break
+          case buf[1].interval > delay:
+            ret = Rx.Observable.of(Object.create(gap), buf[1]);
+            break;
+          default:
+            ret = Rx.Observable.just(buf[1]);
         }
 
         return ret;
       })
-      .map(x => x.gap ? '☺' : x.value.keyCode)
+      .pluck('value')
       .bufferWithCount(sequence.length, 1)
-      .map(buf => ({ buf, sequence })) 
-      .filter(checkSequence)
-      .subscribeOnNext(callback);
+      .filter(x => x.toString() === pattern)
+      .subscribeOnNext(callback)
 
   return sub.dispose.bind(sub);
 };
